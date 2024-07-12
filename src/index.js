779c7bf824c61f995ba015a90bf5c61556f741f1
Blue <blue@whatthe.blue>
2024-07-13 00:51:46 +0100

    rewording about and adding colours

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const path = require('path')
 const app = express()
 
 const port = 3003
-nunjucks.configure(["views","node_modules/govuk-frontend/dist","assets","node_modules/@ministryofjustice/frontend"],{
+nunjucks.configure(["views","node_modules/govuk-frontend/dist","assets","node_modules/@ministryofjustice/frontend","favicon"],{
   express:app,
   autoescape: true
 });
 
 app.use(express.static("assets"))
+app.use("favicon", express.static("favicon"))
 app.use("/govuk-assets",express.static("node_modules/govuk-frontend/dist"))
 app.use("/moj-assets", express.static("node_modules/@ministryofjustice/frontend/moj/"))
 app.set("view engine", "html")

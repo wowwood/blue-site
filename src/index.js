581739404cf1bb9b2a673af9ecd23c947df05bc5
Blue <blue@whatthe.blue>
2024-07-12 20:55:32 +0100

    adding more content

--- src/index.js
+++ src/index.js
@@ -4,22 +4,26 @@ const path = require('path')
 const app = express()
 
 const port = 3003
-nunjucks.configure(["views","node_modules/govuk-frontend/dist","assets"],{
+nunjucks.configure(["views","node_modules/govuk-frontend/dist","assets","node_modules/@ministryofjustice/frontend"],{
   express:app,
   autoescape: true
 });
 
 app.use(express.static("assets"))
 app.use("/govuk-assets",express.static("node_modules/govuk-frontend/dist"))
-//app.use('/assets', express.static(path.join(__dirname, '/node_modules/govuk-frontend/dist/govuk/assets')))
+app.use("/moj-assets", express.static("node_modules/@ministryofjustice/frontend/moj/"))
 app.set("view engine", "html")
 
 app.get('/', (req, res) => {
   res.render('index.html', {
-    time:Date.now()
-
   })
 })
+
+app.get('/names', (req, res) => {
+  res.render('names.html', {
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

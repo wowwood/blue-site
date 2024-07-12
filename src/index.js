f51b69483d8e56e9643e05112faafa8e4e54cdde
Blue <blue@whatthe.blue>
2024-07-12 20:55:32 +0100

    working with gov uk design

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const path = require('path')
 const app = express()
 
 const port = 3003
+nunjucks.configure(["views","node_modules/govuk-frontend/dist","assets"],{
+  express:app,
+  autoescape: true
+});
 
-
+app.use(express.static("assets"))
+app.use("/govuk-assets",express.static("node_modules/govuk-frontend/dist"))
+//app.use('/assets', express.static(path.join(__dirname, '/node_modules/govuk-frontend/dist/govuk/assets')))
+app.set("view engine", "html")
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index.html', {
+    time:Date.now()
+
+  })
 })
 
 app.listen(port, () => {
